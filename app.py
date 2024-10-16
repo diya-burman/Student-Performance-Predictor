8734f37f05b3d43e8b641a4d9e03357bcb3c5307
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-10-16 19:06:08 +0530

    improved frontend

--- app.py
+++ app.py
@@ -37,7 +37,14 @@ def predict_datapoint():
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("After Prediction")
-        return render_template('home.html',results=results[0])
+        return render_template('home.html',results=results[0],
+                                gender=request.form.get('gender'),
+                                ethnicity=request.form.get('ethnicity'),
+                                parental_level_of_education=request.form.get('parental_level_of_education'),
+                                lunch=request.form.get('lunch'),
+                                test_preparation_course=request.form.get('test_preparation_course'),
+                                writing_score=request.form.get('writing_score'),
+                                reading_score=request.form.get('reading_score'))
     
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
